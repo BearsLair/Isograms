5600e82ac5a05866bf648adc49ecae364ac40a44
Patrick Gross <bears_lair@protonmail.com>
2024-04-17 18:23:32 -0700

    initial commit

---
+++ Isograms.js
@@ -0,0 +1,12 @@
+//toLowerCase
+
+function isIsogram(str) {
+  //...
+}
+
+assert.strictEqual(isIsogram("Dermatoglyphics")); //true
+assert.strictEqual(isIsogram("isogram")); //true
+assert.strictEqual(isIsogram("aba")); //false
+assert.strictEqual(isIsogram("moOse")); //false
+assert.strictEqual(isIsogram("isIsogram")); //false
+assert.strictEqual(isIsogram("")); //true
