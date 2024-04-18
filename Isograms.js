b68ac61dea8a71b408cd5be2f8d052cdd0da1b1e
Patrick Gross <bears_lair@protonmail.com>
2024-04-18 00:48:46 -0700

    updated tests

--- Isograms.js
+++ Isograms.js
@@ -1,12 +1,13 @@
 //toLowerCase
+//
 
 function isIsogram(str) {
   //...
 }
 
-assert.strictEqual(isIsogram("Dermatoglyphics")); //true
-assert.strictEqual(isIsogram("isogram")); //true
-assert.strictEqual(isIsogram("aba")); //false
-assert.strictEqual(isIsogram("moOse")); //false
-assert.strictEqual(isIsogram("isIsogram")); //false
-assert.strictEqual(isIsogram("")); //true
+console.log(isIsogram("Dermatoglyphics")); //true
+console.log(isIsogram("isogram")); //true
+console.log(isIsogram("aba")); //false
+console.log(isIsogram("moOse")); //false
+console.log(isIsogram("isIsogram")); //false
+console.log(isIsogram("")); //true
