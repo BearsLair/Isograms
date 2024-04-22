1d61993e44b869295875b55087051eb38fa5791e
Patrick Gross <bears_lair@protonmail.com>
2024-04-22 15:42:23 -0700

    isogram checker complete

--- Isograms.js
+++ Isograms.js
@@ -1,8 +1,17 @@
 //toLowerCase
-//
+//create set with str
+//compare set to str with strict equals
 
 function isIsogram(str) {
-  //...
+  console.log(str);
+  str.toLowerCase();
+  const uniqueChars = new Set(str);
+  uniqueString = [...uniqueChars].join("");
+  if (str === uniqueString) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(isIsogram("Dermatoglyphics")); //true
